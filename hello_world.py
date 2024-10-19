3f7660545a446d1cbadbc6b425c9484826a5f0d7
ZoyaQadeer <zoyaqadeer2507@gmail.com>
2024-10-19 14:51:30 +0530

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,8 @@
 # hello_world.py
-def greet():
-    print("Hello, Hacktoberfest!")
+
+def greet(name="Hacktoberfest"):
+    """Print a greeting to the user."""
+    print(f"Hello, {name}!")
 
 if __name__ == "__main__":
     greet()
